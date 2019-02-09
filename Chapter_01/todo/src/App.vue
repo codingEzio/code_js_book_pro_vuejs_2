fd0284fd417fce9d255e4ad951d208180f43b682
Ezio <assassinste@gmail.com>
2019-02-10 07:26:44 +0800

    Dynamic content (a name & a list of tasks)

--- Chapter_01/todo/src/App.vue
+++ Chapter_01/todo/src/App.vue
@@ -4,12 +4,37 @@
 
 <template>
   <div id="app">
-    <h4 class="bg-primary text-white text-center p-2">TODO List</h4>
+    <h4 class="bg-primary text-white text-center p-2">
+      <!-- Text interpolation binding -->
+      {{ name }}'s TODO List
+    </h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Task</div>
+        <div class="col-2 font-weight-bold">Done</div>
+      </div>
+
+      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+        <div class="col">{{t.action}}</div>
+        <div class="col-2">{{t.done}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "app"
+  name: "app",
+  data() {
+    return {
+      name: "Yorkie",
+      tasks: [
+        { action: "Watch Talk-show", done: false },
+        { action: "Learn Vue.js", done: true },
+        { action: "Rest a while", done: false },
+        { action: "Learn German", done: true }
+      ]
+    };
+  }
 };
 </script>
