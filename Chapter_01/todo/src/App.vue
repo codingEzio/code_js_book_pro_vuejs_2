5127661587e65da1daa5cb2c8e3815869de74467
Ezio <assassinste@gmail.com>
2019-02-10 21:23:40 +0800

    Filtering completed tasks (you do have options: on/off)

--- Chapter_01/todo/src/App.vue
+++ Chapter_01/todo/src/App.vue
@@ -8,17 +8,25 @@
       <!-- Text interpolation binding -->
       {{ name }}'s TODO List
     </h4>
+
     <div class="container-fluid p-4">
       <div class="row">
         <div class="col font-weight-bold">Task</div>
         <div class="col-2 font-weight-bold">Done</div>
       </div>
 
-      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
         <div class="col">{{t.action}}</div>
-        <div class="col-2">
+        <div class="col-2 text-center">
           <input type="checkbox" v-model="t.done" class="form-check-input">
-          {{t.done}}
+        </div>
+      </div>
+
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <!-- Toggling whether it shows the 'completed tasks' or not -->
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input">
+          <label class="form-check-label font-weight-bold">Hide completed tasks</label>
         </div>
       </div>
     </div>
@@ -36,8 +44,18 @@ export default {
         { action: "Learn Vue.js", done: true },
         { action: "Rest a while", done: false },
         { action: "Learn German", done: true }
-      ]
+      ],
+      hideCompleted: true
     };
+  },
+  computed: {
+    // The data will be poured into here
+    //  to computing stuff before being used in 'template'
+    filteredTasks() {
+      // ?  true   ->  tasks that 'to be done'
+      // :  false  ->  all the tasks
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks;
+    }
   }
 };
 </script>
